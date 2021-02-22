7d6922d5eb41774be6e4922f9aba3bf75235d752
adard2002 <adard2002@gmail.com>
2021-02-22 16:08:05 -0600

    added everything from cardOne

--- README.md
+++ README.md
@@ -40,11 +40,11 @@ Follow the naming convention of <\partner 1 initials>-<\partner 2 initials>-book
 ## Credits and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
-02-22-2021 00:00pm - 
+02-22-2021 02:46pm - 00:00pm: Card 1
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
+02-22-2021 00:00pm - 00:00pm:
--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 'use strict';
 
 const express = require('express');
-const { response } = require('express');
+// const superagent = require('superagent');
 const PORT = process.env.PORT || 3111;
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('public')); // loads the public folder (css)
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', getBooks);
+
+// app.get('/book-route', showSearchPage);
+// app.post('/book-search', makeBookSearch);
+// Crate as a post because data is being sent to us.
+// The get describes the page and the post path ties these events together. So we keep them the same.
 
 // If user is taken to the home page the following text will appear: "You have found the home page!"
-app.get('/', (request, response) => {
-  response.send('You have found the home page!');
-});
 
 
 
+function getBooks(req, res){
+  res.render('pages/index');
+}
+// function showSearchPage(req,res){
+//   res.render('error.ejs');
+// }
 
+// function makeBookSearch(req,res){
+//   cost url = `https://www.googleleapis.com/books/v1/volumes?q=+intitle:${title}`;
+// }
+
+// superagent(url)
+// .then(books => {
+//   // console.log(books.body.items);
+
+//   const titles = books.body.map( book => book.volumeInfo.title);
+//   res.render('results.ejs', {titles: titles});
+// });
 
 
 // ====== PORT Listener ======
