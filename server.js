cbc04056533f5d70c10ceb964a049b82dc46ce66
jmcia2020 <jeanmmcmahon@gmail.com>
2021-02-24 17:06:45 -0600

    Description rendering to page

--- server.js
+++ server.js
@@ -7,74 +7,71 @@ const PORT = process.env.PORT || 3000;
 // const savedBookTitles = [];
 
 app.use(express.static('public')); // loads the public folder (css)
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); //tell express to load ejs this unlocks the response render
 
 app.get('/', getBooks);
 app.get('/searches/new', makeBookSearch);
 app.post('/searches', getResults);
-app.post('/save-book', saveBook);
+// app.post('/save-book', saveBook);
 
 // Fail safe routes
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 app.get((error, req, res) => handleError(error, res)); // handle errors
 
 
-
-
-
-function getBooks(req, res){ //home page
+function getBooks(req, res) { //home page
   res.render('pages/index');
 }
-function makeBookSearch(req, res){ // search for book
+function makeBookSearch(req, res) { // search for book
   res.render('pages/searches/new.ejs');
 }
 
 
-function getResults(req, res){
-  const title = req.body.title;
-  const url = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${title}`;
+// function getResults(req, res) {
+//   const title = req.body.title;
+//   const url = `https://www.googleapis.com/books/v1/volumes`;
 
-  superagent(url)
-    .then(books => {
-      const titles = books.body.items.map( book => new Book(book.volumeInfo));
-      console.log(titles);
-      res.render('pages/searches/show', {titles: titles});
-    }).catch(err => handleError(err, res));
-}
+//   superagent.get(url)
+//     .query({
+//       q: `+in${req.body.searchType}:${req.body.searchQuery}`
+//     })
+//     .then(books => {
+//       const titles = books.body.items.map(book => new Book(book.volumeInfo));
+//       res.render('pages/searches/show', { titles: titles });
+//       console.log(title);
+//     }).catch(err => handleError(err, res));
+// }
 
 
-function saveBook(req, res){
-  savedBookTitles.push(req.body.title);
-  res.redirect('/');
-}
+// function saveBook(req, res) {
+//   savedBookTitles.push(req.body.title);
+//   res.redirect('/');
+// }
 
 // Need Constructor function for titles | image, title, author, and desc
-function Book(bookObject){
+function Book(bookObject) {
   this.title = bookObject.title ? bookObject.title : 'Title not found';
   // this.img = bookObject.imageLinks || `https://i.imgur.com/J5LVHEL.jpg`;
   this.author = bookObject.authors || 'Author not found';
-  this.desc = bookObject.description || 'No description provided';
+  this.description = bookObject.description ? bookObject.description : 'No description provided';
 }
 
-function getBooks(req, res){ //home page
-  res.render('pages/index');
-}
 
-function makeBookSearch(req, res){ // search for book
-  res.render('pages/searches/new.ejs');
-}
 
-function getResults(req, res){
+function getResults(req, res) {
   let url = `https://www.googleapis.com/books/v1/volumes?q=`;
-  // console.log(req.body);
-  // if(req.body.searchType === 'title'){ url += `+intitle:${req.body.searchBar}`;}
-  // if(req.body.searchType[1] === 'author'){ url += `+inauthor:${req.body.searchType[0]}`;}
+  if (req.body.searchType === 'title') { url += `+intitle:${req.body.searchBar}`; }
+  if (req.body.searchType === 'author') { url += `+inauthor:${req.body.searchBar}`; }
   superagent.get(url)
     .then(books => books.body.items.map(book => new Book(book.volumeInfo)))
-    .then(results => res.render('pages/searches/show', {results: results}));
+    .then(results => res.render('pages/searches/show', { results: results }))
+    .catch(err => handleError(err, res));
+
+  console.log(req.body);
 }
+
 // function saveBook(req, res){
 //   savedBookTitles.push(req.body.title);
 //   console.log(req.body.title);
@@ -83,11 +80,11 @@ function getResults(req, res){
 
 
 function handleError(error, response) {
-  response.render('pages/error', { error: error});
+  response.render('pages/error', { error: error });
 }
 
 
 
 // ====== PORT Listener ======
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
