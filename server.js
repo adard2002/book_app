ea0178f87bab6537e4fb66612781e3053907eb4e
jmcia2020 <jeanmmcmahon@gmail.com>
2021-02-24 09:43:16 -0600

    cb render to page

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 // const savedBookTitles = [];
-// const limit = 10;
+
 app.use(express.static('public')); // loads the public folder (css)
 app.use(express.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs'); //tell express to load ejs this unlocks the response render
+
 app.get('/', getBooks);
 app.get('/searches/new', makeBookSearch);
+
 app.post('/searches', getResults);
 // app.post('/save-book', saveBook);
+
 // Need Constructor function for titles | image, title, author, and desc
 function Book(bookObject){
   this.title = bookObject.title ? bookObject.title : 'Title not found';
@@ -20,17 +24,20 @@ function Book(bookObject){
   this.author = bookObject.authors || 'Author not found';
   this.desc = bookObject.description || 'No description provided';
 }
+
 function getBooks(req, res){ //home page
   res.render('pages/index');
 }
+
 function makeBookSearch(req, res){ // search for book
   res.render('pages/searches/new.ejs');
 }
+
 function getResults(req, res){
   let url = `https://www.googleapis.com/books/v1/volumes?q=`;
-  console.log(req.body);
+  // console.log(req.body);
   if(req.body.searchType === 'title'){ url += `+intitle:${req.body.searchBar}`;}
-  if(req.body.searchType === 'author'){ url += `+inauthor:${req.body.searchBar}`;}
+  if(req.body.searchType[1] === 'author'){ url += `+inauthor:${req.body.searchType[0]}`;}
   superagent.get(url)
     .then(books => books.body.items.map(book => new Book(book.volumeInfo)))
     .then(results => res.render('pages/searches/show', {results: results}));
@@ -42,3 +49,4 @@ function getResults(req, res){
 // }
 // ====== PORT Listener ======
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
